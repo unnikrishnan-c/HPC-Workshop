4d697630da8eb70cffb6476bb172d4668ccc939b
KevinJudeConcessao <32903427+KevinJudeConcessao@users.noreply.github.com>
2021-03-26 11:06:10 +0530

    Correct indentation.

--- programs/numdevices.c
+++ programs/numdevices.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
-#include<omp.h>
-int main(){
-    int Numdevices=omp_get_num_devices();
-    printf("number of devices= %d", Numdevices);
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+  int Numdevices = omp_get_num_devices();
+  printf("number of devices= %d\n", Numdevices);
 }
