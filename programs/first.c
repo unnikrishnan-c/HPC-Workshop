69bf48a82e10cf923f75ea93de572ebf45bd6766
KevinJudeConcessao <32903427+KevinJudeConcessao@users.noreply.github.com>
2021-03-26 11:31:55 +0530

    Fix formatting + pragma
    Change #pragma omp target enter data to #pragma omp target data : target enter clause does not allow from map modifer; only to and alloc modifiers allowed.

--- programs/first.c
+++ programs/first.c
@@ -1,35 +1,41 @@
-#include<stdio.h>
-#include<omp.h>
-#include<stdlib.h>
-#define n 1024
-int A[n][n], B[n][n], C[n][n];
-void check(){
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-for(int i=0; i<n; i++) {
-for(int j=0; j<n; j++){
-if (C[i][j] != (A[i][j] * B[i][j])){
-	printf("error %d %d\n", i,j);break;
+#define N 1024
+
+int A[N][N], B[N][N], C[N][N];
+
+void check() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (C[i][j] != (A[i][j] * B[i][j])) {
+        printf("[ERROR] %d %d\n", i, j);
+        break;
+      }
+    }
+  }
 }
-}
-}
-}
-int main(){
-srand(3);
-for(int i=0;i<n;i++){
-	for(int j=0;j<n;j++){
-		A[i][j]=rand()%1024+1;
-		B[i][j]=rand()%1024+1;
-	}
-}
-#pragma omp target enter data map(to: A, B) (from: C)
-{
+
+int main() {
+  srand(3);
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      A[i][j] = rand() % 1024 + 1;
+      B[i][j] = rand() % 1024 + 1;
+    }
+  }
+
+#pragma omp target data map(to : A, B) map(from : C)
+  {
 #pragma omp parallel for
-for(int i=0; i<n; i++) {
-for(int j=0; j<n; j++)
-C[i][j] = A[i][j] * B[i][ j];
-}
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+        C[i][j] = A[i][j] * B[i][j];
+    }
+  }
 
-}
-printf("%d %d %d \n",A[0][0],B[0][0], C[0][0]);
-check();
+  printf("%d %d %d \n", A[0][0], B[0][0], C[0][0]);
+  check();
 }
